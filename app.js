fa58dc1b4454e619d7814696ef500488466c3e9f
Kiran_Kumar_Gangadharaiah <gangadharaiah.k@husky.neu.edu>
2020-05-10 11:38:24 -0400

    Added templates Pug and Handlebars

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
 //setting up pug templating engine and load pug
-app.set('view engine', 'pug');
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname : 'hbs'
+    })
+);
+//registerd as handlebars
+app.set('view engine', 'hbs');
 app.set('views','views');
 
 const adminData = require('./routes/admin');
@@ -21,7 +31,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404',{pageTitle:'Page not found'});
 });
 
 
