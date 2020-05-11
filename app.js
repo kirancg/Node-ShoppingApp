5aa3a7c8032e09e24bb9eabb62a406d1f31d2427
Kiran_Kumar_Gangadharaiah <kirancg93@gmail.com>
2020-05-11 11:19:18 -0400

    Added MVC Model

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-//const http = require('http');
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// //setting up pug templating engine and load pug
-// app.engine(
-//     'hbs',
-//     expressHbs({
-//         layoutsDir: 'views/layouts/',
-//         defaultLayout: 'main-layout',
-//         extname : 'hbs'
-//     })
-// );
-
-
-//registerd as handlebars
 app.set('view engine', 'ejs');
-app.set('views','views');
+app.set('views', 'views');
 
-const adminData = require('./routes/admin');
-
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-//Serve statically
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404',{pageTitle:'Page not found'});
-});
-
-
+app.use(errorController.get404);
 
 app.listen(3000);
