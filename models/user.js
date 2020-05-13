ba4c3ead141807f55246c9c840713f837df76f29
Kiran_Kumar_Gangadharaiah <kirancg93@gmail.com>
2020-05-13 01:15:19 -0400

    Completed the cart

--- models/user.js
+++ models/user.js
@@ -60,7 +60,7 @@ userSchema.methods.clearCart = function() {
   this.cart = { items: [] };
   return this.save();
 };
-
+  
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
